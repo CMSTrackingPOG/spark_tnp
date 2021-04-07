18e7a71f38d05e312c3b6f852a46d8ba54ee7cfb
Andre Frankenthal <afrankenthal@gmail.com>
2021-04-07 04:24:26 +0200

    Added RooCMSShape compilation to environment source script

--- env-edge-node.sh
+++ env-edge-node.sh
@@ -1,4 +1,13 @@
+echo "Sourcing environment for hadoop edge node..."
+
 LCG_RELEASE=LCG_97apython3
 LCG_ARCH=x86_64-centos7-gcc8
 source /cvmfs/sft.cern.ch/lcg/views/${LCG_RELEASE}/${LCG_ARCH}-opt/setup.sh
 source hadoop-setconf.sh analytix
+
+if [ ! -f RooCMSShape_cc.so ]; then
+	echo ""
+	echo "Did not detect a RooCMSShape shared object file. Compiling with ACLiC... (should be needed only once)"
+	root -l -b -q -e '.L RooCMSShape.cc+'
+	echo "Done!"
+fi
--- env-lxplus.sh
+++ env-lxplus.sh
@@ -1,4 +1,13 @@
+echo "Sourcing environment for lxplus node..."
+
 LCG_RELEASE=LCG_97apython3
 LCG_ARCH=x86_64-centos7-gcc8
 source /cvmfs/sft.cern.ch/lcg/views/${LCG_RELEASE}/${LCG_ARCH}-opt/setup.sh
 source /cvmfs/sft.cern.ch/lcg/etc/hadoop-confext/hadoop-swan-setconf.sh analytix
+
+if [ ! -f RooCMSShape_cc.so ]; then
+	echo ""
+	echo "Did not detect a RooCMSShape shared object file. Compiling with ACLiC... (should be needed only once)"
+	root -l -b -q -e '.L RooCMSShape.cc+'
+	echo "Done!"
+fi
